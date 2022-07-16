b6ce7738a4aa8a45dc07cc5c9a3228575b326fa7
Angelo  Menti <angelo.omarzinho@outlook.com>
2022-07-15 22:57:07 -0300

    dataSource - fixed host

--- src/data-source.ts
+++ src/data-source.ts
@@ -6,7 +6,7 @@ require("dotenv").config();
 
 export const AppDataSource = new DataSource({
   type: "postgres",
-  host: "localhost",
+  host: process.env.TYPEORM_HOST,
   port: 5432,
   username: process.env.TYPEORM_USERNAME,
   password: process.env.TYPEORM_PASSWORD,
